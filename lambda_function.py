e73af5812c3f7502f9fcd2b720250bc10530ea6c
Floyd Hightower <floyd.hightower27@gmail.com>
2023-06-08 15:53:47 -0400

    Capture exception in Sentry

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,21 @@
+import os
+
 import sentry_sdk
+
+sentry_sdk.init(
+    dsn=os.getenv('sentry_dsn'),
+    traces_sample_rate=1.0,
+)
 
 
 def lambda_handler(event, context):
     query_strings = event.get('queryStringParameters') or {}
     count = 1
     if qs_count := query_strings.get('count'):
-        count = int(qs_count)
+        try:
+            count = int(qs_count)
+        except ValueError as e:
+            sentry_sdk.capture_exception(e)
 
     response = {
         'statusCode': 200,
